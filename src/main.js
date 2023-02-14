57a83c4a5ad468c67ed23e756ffd20510986a3f1
Mohamed-Gamal00 <gmohamed777@yahoo.com>
2023-02-14 18:52:47 +0200

    vuetify testing

--- src/main.js
+++ src/main.js
@@ -2,5 +2,26 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-
-createApp(App).use(store).use(router).mount("#app");
+/* vue-snip */
+import VueSnip from "vue-snip";
+// Vuetify
+import "vuetify/styles";
+import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+import "material-design-icons-iconfont/dist/material-design-icons.css";
+import "@mdi/font/css/materialdesignicons.css";
+/* sweet alert */
+import VueSweetalert2 from "vue-sweetalert2";
+import "sweetalert2/dist/sweetalert2.min.css";
+const vuetify = createVuetify({
+  components,
+  directives,
+});
+createApp(App)
+  .use(store)
+  .use(VueSweetalert2)
+  .use(VueSnip)
+  .use(vuetify)
+  .use(router)
+  .mount("#app");
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,99 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <v-container>
+    <v-row>
+      <v-col v-for="servise in services.slice(3)" :key="servise">
+        <v-card class="mx-end" max-width="344">
+          <v-img :src="servise.image" height="200px" contain></v-img>
+
+          <v-card-title> Top western road trips </v-card-title>
+
+          <v-card-subtitle> 1,000 miles of wonder </v-card-subtitle>
+
+          <v-card-actions>
+            <v-btn @click="showalert()" color="orange-lighten-2" variant="text">
+              Explore
+            </v-btn>
+
+            <v-spacer></v-spacer>
+
+            <v-btn
+              :icon="show ? 'mdi-chevron-up' : 'mdi-chevron-down'"
+              @click="show = !show"
+            ></v-btn>
+          </v-card-actions>
+
+          <v-expand-transition>
+            <div v-show="show">
+              <v-divider></v-divider>
+
+              <v-card-text>
+                I'm a thing. But, like most politicians, he promised more than
+                he could deliver. You won't have time for sleeping, soldier, not
+                with all the bed making you'll be doing. Then we'll go with that
+                data file! Hey, you add a one and two zeros to that or we walk!
+                You're going to do his laundry? I've got to find a way to
+                escape.
+              </v-card-text>
+            </div>
+          </v-expand-transition>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
+
+<script>
+import axios from "axios";
+export default {
+  data: () => ({
+    show: false,
+    services: [],
+    messages: [
+      {
+        from: "You",
+        message: `Sure, I'll see you later.`,
+        time: "10:42am",
+        color: "deep-purple-lighten-1",
+      },
+      {
+        from: "John Doe",
+        message: "Yeah, sure. Does 1:00pm work?",
+        time: "10:37am",
+        color: "green",
+      },
+      {
+        from: "You",
+        message: "Did you still want to grab lunch today?",
+        time: "9:47am",
+        color: "deep-purple-lighten-1",
+      },
+    ],
+  }),
+  mounted() {
+    this.featharticles();
+  },
+  methods: {
+    showalert() {
+      this.$swal("Hello vuetify world!!!");
+    },
+    async featharticles() {
+      /* servcies */
+      await axios
+        .get(`https://admin.sigma-tech.agency/api/services`)
+        .then((response) => {
+          this.services = response.data.services;
+          console.log(this.services);
+        })
+        .catch(() => this.$router.push({ name: "servererror" }));
+      // if (services.status == 200) {
+      //   this.services = services.data.services;
+      // }
+    },
+  },
+};
+</script>
+<style scoped>
+* {
+  direction: rtl;
+}
+</style>
